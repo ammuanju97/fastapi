b6c0acd4edd31686106023a537b19f9bf1874192
ammuanju97 <anjutennyson@gmail.com>
2021-10-25 15:49:51 +0530

    fastapi modified

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ def about():
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
+    
